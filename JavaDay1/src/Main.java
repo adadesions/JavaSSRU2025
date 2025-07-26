80053f0680fe05fc0265c31370a99f722972b9b5
AdaDeSions <adadesions@gmail.com>
2025-07-26 10:55:25 +0700

    init project

---
+++ JavaDay1/src/Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static float pythagoreanTheorem(float a, float b) {
+        return (float) Math.sqrt(a * a + b * b);
+    }
+
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    private static void printMinNumber(double v1, double v2) {
+        double a = v1;
+        double b = v2;
+        double m = 0;
+
+        String r = "The minimum of numbers: %.2f and %.2f is %.2f";
+
+        m = Math.min(a, b);
+
+        String result = String.format(r, a, b, m);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        float c = pythagoreanTheorem(3, 4);
+        System.out.println("The hypotenuse is: " + c);
+
+        boolean isEven = isEven(22);
+        System.out.println("Is 22 even? : " + isEven);
+
+        printMinNumber(4.5, 3.2);
+    }
+}
